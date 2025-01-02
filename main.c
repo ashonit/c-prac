1c06c63441d859bb16bc45d84616cea4fae1e44a
Ash <ashonit@gmail.com>
2025-01-02 23:43:34 +1030

    Added some more tests

--- main.c
+++ main.c
@@ -1,54 +1,61 @@
 #include <stdio.h>
 #include "myfunc.h"
 
-void testAdd()
+void testAdd(int a, int b, const int expected)
 {
-  const int expected = 16;
-  int result = add(5, 11);
+  int result = add(a, b);
   if (result == expected)
   {
-    printf("Add got expected value %d.\n", expected);
+    printf("Add (%d + %d) got expected value %d.\n", a, b, expected);
   }
   else
   {
-    printf("Add got incorrect result %d, expecting %d.\n", result, expected);
+    printf("Add (%d + %d) got incorrect result %d, expecting %d.\n", a, b, result, expected);
   }
 }
 
-void testSubtract()
+void testSubtract(int a, int b, const int expected)
 {
-  const int expected = -6;
-  int result = subtract(5, 11);
+  int result = subtract(a, b);
   if (result == expected)
   {
-    printf("Subtract got expected value %d.\n", expected);
+    printf("Subtract (%d - %d) got expected value %d.\n", a, b, expected);
   }
   else
   {
-    printf("Subtract got incorrect result %d, expecting %d.\n", result, expected);
+    printf("Subtract (%d - %d) got incorrect result %d, expecting %d.\n", a, b, result, expected);
   }
 }
 
-void testAdd_f()
+void testAdd_f(float a, float b, const float expected)
 {
-  const float expected = 10.5;
-  float result = add_f(4.2, 6.3);
+  //const float expected = 10.5;
+  float result = add_f(a, b);
   if (result == expected)
   {
-    printf("Add_f got expected value %f.\n", expected);
+    printf("Add_f (%f + %f) got expected value %f.\n", a, b, expected);
   }
   else
   {
-    printf("Add_f got incorrect result %f, expecting %f.\n", result, expected);
+    printf("Add_f (%f + %f) got incorrect result %f, expecting %f.\n", a, b, result, expected);
   }
 }
 
 int main(int argc, char *argv[])
 {
   hello();
-  testAdd();
-  testSubtract();
-  testAdd_f();
+  testAdd(5, 11, 16);
+  testAdd(-4, 7, 3);
+  testAdd(99, -101, -2);
+  testAdd(-2, -3, -5);
+  testSubtract(10, 8, 2);
+  testSubtract(5, 11, -6);
+  testSubtract(-8, -3, -11);
+  testSubtract(8, -3, 11);
+  testAdd_f(4.2, 6.3, 10.5);
+  testAdd_f(-4.2, 6.3, 2.1);
+  testAdd_f(8.8, -6.3, 2.5);
+  testAdd_f(-2.6, -101.2, -103.8);
 }
 
 
