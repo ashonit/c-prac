fc5201842517134ff04621f0de51964bada78f84
Ash <ashonit@gmail.com>
2025-03-09 17:12:14 +1030

    Fixed wrong test

--- main.c
+++ main.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
   testAdd(-2, -3, -5);
   testSubtract(10, 8, 2);
   testSubtract(5, 11, -6);
-  testSubtract(-8, -3, -11);
+  testSubtract(-8, -3, -5);
   testSubtract(8, -3, 11);
   testAdd_f(4.2, 6.3, 10.5);
   testAdd_f(-4.2, 6.3, 2.1);
